6234d6e400e97ebdd2c11e48d53a39e76446c6cc
Zack <lzcers@gmail.com>
2019-07-12 01:17:25 +0800

    first blood

---
+++ genTagslist.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+const fm = require('front-matter');
+
+const genTagslist = postsDir => {
+    // 拿到所有的文章
+    const arrPosts = fs
+        .readdirSync(postsDir)
+        .filter(i => !i.match(/(.json|imgs)/))
+        .reverse();
+
+    return Promise.all(
+        arrPosts.map(
+            (i, index) =>
+                new Promise((resolve, reject) => {
+                    fs.readFile(
+                        path.format({ dir: postsDir, base: i }),
+                        'utf8',
+                        (err, data) => {
+                            if (err) reject(err);
+                            const { attributes, body } = fm(data);
+                            // 替换博文中的图片链接
+                            fs.writeFileSync(
+                                path.format({ dir: postsDir, base: i }),
+                                data.replace(
+                                    /\]\(\.(\\|\/)imgs(\\|\/)/g,
+                                    '](/articles/imgs/'
+                                ),
+                                'utf8'
+                            );
+                            resolve({
+                                fileName: i,
+                                ID: index,
+                                ...attributes,
+                                Content:
+                                    body
+                                        .replace(
+                                            /\]\(\.(\\|\/)imgs(\\|\/)/g,
+                                            '](/articles/imgs/'
+                                        )
+                                        .split(/(。)/g, 10)
+                                        .join('') + '<strong>……</strong>'
+                            });
+                        }
+                    );
+                })
+        )
+    );
+};
+
+module.exports = {
+    genTagslist
+};
---
+++ pubTool.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+
+const { exec, execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const { delFolder, copyFolder } = require('./utils.js');
+const { genTagslist } = require('./genTagslist.js');
+
+// 源目录
+const sourcePath = path.format({ dir: 'C:\\code\\docs\\' });
+const sourceDocPath = path.format({ dir: 'C:\\code\\docs\\blog' });
+// 目标目录
+const targetPath = path.format({ dir: 'C:\\code\\new_blog\\' });
+// 文档目录
+const destPath = path.format({ dir: 'C:\\code\\new_blog\\docs\\articles' });
+
+const argv = process.argv.slice(2);
+// 第一个参数是命令， 后面跟命令的参数
+// 只实现 commandName arg1 arg2 arg3 ... 的形式
+const [commandName, ...args] = argv;
+
+// save to repository
+const cSaveToRepo = () => {
+    console.log('----------------------------------------');
+    console.log('正在保存文章至远端仓库...');
+    try {
+        const add = 'git add .',
+            commit = `git commit -m "update posts..."`,
+            push = 'git push';
+        console.log(add);
+        execSync(add, { cwd: sourcePath });
+        console.log(commit);
+        execSync(commit, { cwd: sourcePath });
+        console.log(push);
+        execSync(push, { cwd: sourcePath });
+    } catch (e) {
+        console.log('推送失败!' + e.stdout);
+    }
+};
+
+const cPublish = () => {
+    const push = () => {
+        // 生成 tags
+        genTagslist(destPath).then(tagsList => {
+            fs.writeFile(
+                path.format({
+                    dir: destPath,
+                    base: '/tags.json'
+                }),
+                JSON.stringify(tagsList, null, '  '),
+                'utf8',
+                err => {
+                    if (err) throw err;
+                    console.log('tagsList created success...');
+                    console.log('----------------------------------------');
+                    console.log('正在推送文章...');
+                    try {
+                        const add = 'git add .',
+                            commit = 'git commit -m "update posts..."',
+                            push = 'git push';
+                        execSync(add, { cwd: targetPath });
+                        console.log(commit);
+                        execSync(commit, { cwd: targetPath });
+                        console.log(push);
+                        execSync(push, { cwd: targetPath });
+                    } catch (e) {
+                        console.log('推送失败!');
+                    }
+                }
+            );
+        });
+    };
+    // 1.先干掉文件夹
+    delFolder(destPath);
+    if (!fs.existsSync(destPath)) {
+        fs.mkdir(destPath, function(err) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            // 2. 然后重新拷贝，并生成 tags 调用 git push 操作
+            copyFolder(sourceDocPath, destPath, push);
+        });
+    }
+};
+
+const commandList = {
+    s: cSaveToRepo,
+    p: cPublish,
+    sp: () => (cSaveToRepo(), cPublish())
+};
+
+// 执行命令
+if (!!commandName) {
+    const command = commandList[commandName];
+    command && command(args);
+}
