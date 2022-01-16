138fb8c687fd68f777eaaaaa56bbdbdc09af8140
Zack <lzcers@gmail.com>
2022-01-16 20:31:54 +0800

    update

--- genTagslist.js
+++ genTagslist.js
@@ -31,7 +31,7 @@ const genTagslist = postsDir => {
                             resolve({
                                 file_name: i,
                                 id: index,
-                                title: attributes,
+                                title: attributes.title,
                                 tags: attributes.Tags,
                                 publish_date: attributes.PublishDate,
                             
