5038657f52b57de3b38054f9a6c3900d27802ead
Zack <lzcers@gmail.com>
2026-07-14 21:11:17 +0800

    update

--- dist/utils.js
+++ dist/utils.js
@@ -39,7 +39,7 @@ function replacePostAssetUrl(postStr) {
     return postStr.replace(/!\[([^\]]*)\]\((?!http|https|\/)(.*?)\)/g, (match, altText, url) => {
         const imageName = url.trim().split(/\\|\//).pop()?.trim();
         if (imageName) {
-            return `![${altText}](articles/imgs/${imageName})`;
+            return `![${altText}](articles/assets/${imageName})`;
         }
         return match;
     });
